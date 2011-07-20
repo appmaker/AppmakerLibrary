a83965e895b771dce84a2229f83e5e7fc7129121
Bernhard Harrer <bernhard.harrer@appmaker.de>
2011-07-20 11:32:27 +0200

    Unit Test test

---
+++ AppmakerLibraryTests/AMAlertViewTest.h
@@ -0,0 +1,18 @@
+//
+//  AMAlertViewTest.h
+//  AppmakerLibrary
+//
+//  Created by Bernhard Harrer on 20.07.11.
+//  Copyright 2011 Appmaker e.K. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+
+
+@interface AMAlertViewTest : SenTestCase {
+    
+}
+
+
+@end
---
+++ AppmakerLibraryTests/AMAlertViewTest.m
@@ -0,0 +1,19 @@
+//
+//  AMAlertViewTest.m
+//  AppmakerLibrary
+//
+//  Created by Bernhard Harrer on 20.07.11.
+//  Copyright 2011 SeitWerk. All rights reserved.
+//
+
+#import "AMAlertViewTest.h"
+
+
+@implementation AMAlertViewTest
+
+- (void) testFail {
+    
+    STFail(@"Fail ");
+}
+
+@end
